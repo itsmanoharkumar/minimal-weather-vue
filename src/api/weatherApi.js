3c2da7503eb388ede15239b8b7f590c592105f68
manohar-eros <itsmanoharkumar@gmail.com>
2020-11-27 10:25:19 +0530

    new api key

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -9,7 +9,7 @@ import * as axios from "axios";
 
 export default async function fetchWeather({ lat, lon }) {
   const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/onecall?units=metric&lat=${lat}&lon=${lon}&exclude=minutely&appid=9fc751719e16f8c2ba8aecda28bf3ded`
+    `https://api.openweathermap.org/data/2.5/onecall?units=metric&lat=${lat}&lon=${lon}&exclude=minutely&appid=939cad0dafade96a308ae4f6c8157605`
   );
   return response.data;
 }
